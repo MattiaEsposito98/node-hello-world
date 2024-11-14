9470b5106bce9ddb411932b6dda21ac8721f0383
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-14 15:32:59 +0100

    add codice per recuperare argomento da terminale

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 console.log ("Hello world")
+
+// Per eseguire durante --watch
 // const a = "Hello"
 // const b = "Boolean"
-
 // console.log (a +" "+ b)
+
+
+// Recuperare argomento passato da terminale 
+console.log (process.argv)
+const parola = process.argv.slice(2)
+console.log (parola)
+console.log (`Hello ${parola}`)
